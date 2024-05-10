4b33c35e8d8918a14f9a493fb1809833c9d08a62
TSIDIANY Raharison Muriel <106076190+ITDevSuccess@users.noreply.github.com>
2024-05-10 13:49:07 +0300

    Ajout de donnee dans la base et import des CVS

--- home/admin.py
+++ home/admin.py
@@ -1,52 +1,95 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin, ExportActionMixin
-from import_export.formats.base_formats import XLSX, CSV
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from .models import HardSkill, SoftSkill, JobOffer, JobHardSkill, JobSoftSkill, User, UserSoftSkill, UserHardSkill
 
 
-class CustomImportExportMixin(ExportActionMixin):
-    formats = [XLSX, CSV]
+# Define resources for import-export
+class HardSkillResource(resources.ModelResource):
+    class Meta:
+        model = HardSkill
 
 
+class SoftSkillResource(resources.ModelResource):
+    class Meta:
+        model = SoftSkill
+
+
+class JobOfferResource(resources.ModelResource):
+    class Meta:
+        model = JobOffer
+
+
+class JobHardSkillResource(resources.ModelResource):
+    class Meta:
+        model = JobHardSkill
+
+
+class JobSoftSkillResource(resources.ModelResource):
+    class Meta:
+        model = JobSoftSkill
+
+
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+
+
+class UserSoftSkillResource(resources.ModelResource):
+    class Meta:
+        model = UserSoftSkill
+
+
+class UserHardSkillResource(resources.ModelResource):
+    class Meta:
+        model = UserHardSkill
+
+
+# Register models with import-export functionality
 @admin.register(HardSkill)
-class HardSkillAdmin(CustomImportExportMixin, ImportExportModelAdmin):
-    list_display = ('name',)
+class HardSkillAdmin(ImportExportModelAdmin):
+    resource_class = HardSkillResource
+    list_display = ('id', 'name')
 
 
 @admin.register(SoftSkill)
-class SoftSkillAdmin(CustomImportExportMixin, ImportExportModelAdmin):
-    list_display = ('name',)
-
-
-class JobHardSkillInline(admin.TabularInline):
-    model = JobHardSkill
-
-
-class JobSoftSkillInline(admin.TabularInline):
-    model = JobSoftSkill
+class SoftSkillAdmin(ImportExportModelAdmin):
+    resource_class = SoftSkillResource
+    list_display = ('id', 'name')
 
 
 @admin.register(JobOffer)
-class JobOfferAdmin(CustomImportExportMixin, ImportExportModelAdmin):
-    inlines = [
-        JobHardSkillInline,
-        JobSoftSkillInline,
-    ]
-    list_display = ('name', 'experience')
+class JobOfferAdmin(ImportExportModelAdmin):
+    resource_class = JobOfferResource
+    list_display = ('id', 'name', 'experience')
 
 
-class UserSoftSkillInline(admin.TabularInline):
-    model = UserSoftSkill
+@admin.register(JobHardSkill)
+class JobHardSkillAdmin(ImportExportModelAdmin):
+    resource_class = JobHardSkillResource
+    list_display = ('id', 'job', 'hardskill', 'level')
 
 
-class UserHardSkillInline(admin.TabularInline):
-    model = UserHardSkill
+@admin.register(JobSoftSkill)
+class JobSoftSkillAdmin(ImportExportModelAdmin):
+    resource_class = JobSoftSkillResource
+    list_display = ('id', 'job', 'softskill', 'level')
 
 
 @admin.register(User)
-class UserAdmin(CustomImportExportMixin, ImportExportModelAdmin):
-    inlines = [
-        UserHardSkillInline,
-        UserSoftSkillInline,
-    ]
-    list_display = ('email', 'first_name', 'last_name', 'experience')
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+    list_display = ('id', 'email', 'first_name', 'last_name', 'experience')
+
+
+@admin.register(UserSoftSkill)
+class UserSoftSkillAdmin(ImportExportModelAdmin):
+    resource_class = UserSoftSkillResource
+    list_display = ('id', 'user', 'softskill', 'level')
+
+
+@admin.register(UserHardSkill)
+class UserHardSkillAdmin(ImportExportModelAdmin):
+    resource_class = UserHardSkillResource
+    list_display = ('id', 'user', 'hardskill', 'level')
--- home/models.py
+++ home/models.py
@@ -5,15 +5,24 @@ from django.db import models
 class HardSkill(models.Model):
     name = models.CharField("Savoir-faire", max_length=250)
 
+    def __str__(self):
+        return self.name
+
 
 class SoftSkill(models.Model):
     name = models.CharField("Savoir-être", max_length=250)
 
+    def __str__(self):
+        return self.name
 
-# JOB OFFER 
+
+# JOB OFFER
 class JobOffer(models.Model):
     name = models.CharField("Intitulé du poste", max_length=250)
     experience = models.IntegerField('Expérience souhaitée', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class JobHardSkill(models.Model):
@@ -21,11 +30,17 @@ class JobHardSkill(models.Model):
     hardskill = models.ForeignKey(HardSkill, verbose_name="Hard skills souhaité", on_delete=models.CASCADE)
     level = models.IntegerField("Niveau de compétence souhaité", default=1)  # 1 à 5
 
+    def __str__(self):
+        return f'{self.job} : {self.hardskill}'
+
 
 class JobSoftSkill(models.Model):
     job = models.ForeignKey(JobOffer, on_delete=models.CASCADE, related_name='job_softskills')
     softskill = models.ForeignKey(SoftSkill, verbose_name="Soft skill souhaité", on_delete=models.CASCADE)
     level = models.IntegerField("Niveau de compétence souhaité", default=1)  # 1 à 5
+
+    def __str__(self):
+        return f'{self.job} : {self.softskill}'
 
 
 # USER
@@ -35,14 +50,23 @@ class User(models.Model):
     last_name = models.CharField('Nom', max_length=100, null=True, blank=True)
     experience = models.IntegerField('Années expérience', default=0)
 
+    def __str__(self):
+        return self.first_name
+
 
 class UserSoftSkill(models.Model):
     user = models.ForeignKey(User, verbose_name="Utilisateur", on_delete=models.CASCADE)
     softskill = models.ForeignKey(SoftSkill, verbose_name='Soft skill', on_delete=models.CASCADE)
     level = models.IntegerField("Note / niveau", default=1)  # 1 à 5
 
+    def __str__(self):
+        return f'{self.user} : {self.softskill}'
+
 
 class UserHardSkill(models.Model):
     user = models.ForeignKey(User, verbose_name="Utilisateur", on_delete=models.CASCADE)
     hardskill = models.ForeignKey(HardSkill, verbose_name="Hard skill", on_delete=models.PROTECT)
     level = models.IntegerField("Note / niveau", default=1)  # 1 à 5
+
+    def __str__(self):
+        return f'{self.user} : {self.hardskill}'
